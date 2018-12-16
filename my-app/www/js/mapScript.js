351887a0d4be582d20f7cee2426337e080d717dd
Kasper <kasper_vegte@hotmail.com>
2018-12-16 23:29:39 +0100

    added markers

--- my-app/www/js/mapScript.js
+++ my-app/www/js/mapScript.js
@@ -10,3 +10,31 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
     'Imagery © <a href="http://mapbox.com">Mapbox</a>',
   id: 'mapbox.streets'
 }).addTo(mymap);
+
+app.request.get('https://opendata.arcgis.com/datasets/5aef2dab5bc54db2bdc257011f3a426c_0.geojson', function (data) {
+  var geoObject = JSON.parse(data);
+  var features = [];
+
+  features = geoObject.features;
+  console.log(features);
+
+  var LeafIcon = L.Icon.extend({
+    options: {
+      iconSize:     [38, 95],
+      shadowSize:   [50, 64],
+      iconAnchor:   [22, 94],
+      shadowAnchor: [4, 62],
+      popupAnchor:  [-3, -76]
+    }
+  });
+
+  var greenIcon = new LeafIcon({
+    iconUrl: 'http://leafletjs.com/examples/custom-icons/leaf-green.png',
+    shadowUrl: 'http://leafletjs.com/examples/custom-icons/leaf-shadow.png'
+  });
+
+  features.forEach(function (value) {
+    var geometryArray = [value["geometry"]["coordinates"][1], value["geometry"]["coordinates"][0]];
+    L.marker(geometryArray, {icon: greenIcon}).addTo(mymap);
+  });
+});
